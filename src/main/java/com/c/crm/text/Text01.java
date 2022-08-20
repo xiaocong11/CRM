b1c6067864d6628a5aebd478b1e32174c8f1c140
xiaocong321 <1692671389@qq.com>
2022-08-20 19:06:41 +0800

    添加操作

--- src/main/java/com/c/crm/text/Text01.java
+++ src/main/java/com/c/crm/text/Text01.java
@@ -4,5 +4,6 @@ public class Text01 {
     public static void main(String[] args) {
         System.out.println("你好啊！");
         System.out.println("哈哈哈");
+        System.out.println("嘻嘻嘻");
     }
 }
